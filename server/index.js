f0109991a4d607cdf8f6b6b0711c586956eba7f0
Jesse Nungaray Jr <jrnungaray@hotmail.com>
2024-04-23 17:46:24 -0700

    Refactor server/index.js to improve CORS configuration and error handling

--- server/index.js
+++ server/index.js
@@ -34,63 +34,6 @@ const allowedOrigins = [
   "http://localhost:5173",
 ];
 
-// Configure CORS
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      console.log("Origin:", origin);
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
-
-//Middleware
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "script-src 'self' https://js.stripe.com; img-src 'self' https://q.stripe.com data: https://shorturl.at/cpJWX https://shorturl.at/bL359 https://shorturl.at/qrvE8 https://shorturl.at/ilrGV https://shorturl.at/uwNOX https://shorturl.at/cstU5 https://shorturl.at/iruU8 https://shorturl.at/lmEGX https://tinyurl.com/24o53crt https://m.stripe.network https://m.stripe.com https://b.stripecdn.com"
-  );
-  next();
-});
-
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"))
-);
-app.use(
-  "/assets",
-  express.static(path.join(__dirname, "../client/dist/assets"))
-);
-
-app.use(express.json());
-
-// Use route files as middleware
-app.use("/api", authRoutes);
-app.use("/api", stripeRoutes);
-app.use("/api", productRoutes);
-app.use("/api", orderRoutes);
-app.use("/api", cartRoutes);
-app.use("/api/admin", adminRoutes);
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  // Log the error stack trace to the console
-  console.error(err.stack);
-
-  // If the environment is development, send the error message and stack trace, else send only the error message
-  if (process.env.NODE_ENV === "development") {
-    res.status(500).send({ error: err.message, stack: err.stack });
-  } else {
-    res.status(500).send({ error: err.message });
-  }
-});
-
 // Function to initialize the app
 const init = async () => {
   // Set the port
@@ -108,6 +51,63 @@ const init = async () => {
   // Create the admin user
   await db.createAdminAccount();
   console.log("Admin user created");
+
+  // Configure CORS
+  app.use(
+    cors({
+      origin: function (origin, callback) {
+        console.log("Origin:", origin);
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+          var msg =
+            "The CORS policy for this site does not allow access from the specified Origin.";
+          return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+      },
+    })
+  );
+
+  //Middleware
+  app.use((req, res, next) => {
+    res.setHeader(
+      "Content-Security-Policy",
+      "script-src 'self' https://js.stripe.com; img-src 'self' https://q.stripe.com data: https://shorturl.at/cpJWX https://shorturl.at/bL359 https://shorturl.at/qrvE8 https://shorturl.at/ilrGV https://shorturl.at/uwNOX https://shorturl.at/cstU5 https://shorturl.at/iruU8 https://shorturl.at/lmEGX https://tinyurl.com/24o53crt https://m.stripe.network https://m.stripe.com https://b.stripecdn.com"
+    );
+    next();
+  });
+
+  app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"))
+  );
+  app.use(
+    "/assets",
+    express.static(path.join(__dirname, "../client/dist/assets"))
+  );
+
+  app.use(express.json());
+
+  // Use route files as middleware
+  app.use("/api", authRoutes);
+  app.use("/api", stripeRoutes);
+  app.use("/api", productRoutes);
+  app.use("/api", orderRoutes);
+  app.use("/api", cartRoutes);
+  app.use("/api/admin", adminRoutes);
+
+  // Error handling middleware
+  app.use((err, req, res, next) => {
+    // Log the error stack trace to the console
+    console.error(err.stack);
+
+    // If the environment is development, send the error message and stack trace, else send only the error message
+    if (process.env.NODE_ENV === "development") {
+      res.status(500).send({ error: err.message, stack: err.stack });
+    } else {
+      res.status(500).send({ error: err.message });
+    }
+  });
 
   // Start the app
   app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
